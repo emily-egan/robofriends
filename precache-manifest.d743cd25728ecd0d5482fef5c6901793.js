ee27f2673d84ed7c22143ef270e778d70c92f8cb
Emily Egan <emilyegan00@gmail.com>
2020-02-24 10:12:22 -0500

    Updates

---
+++ precache-manifest.d743cd25728ecd0d5482fef5c6901793.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a323a819e8b488aafc9dc0fa0808f3c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b6634dcc7414b529e374",
+    "url": "/robofriends/static/css/main.b768e9e2.chunk.css"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/robofriends/static/js/2.57b720db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriends/static/js/2.57b720db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6634dcc7414b529e374",
+    "url": "/robofriends/static/js/main.9575312a.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
